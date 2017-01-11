90efe8806d8b82b55be1bf98d81cd8b2c493a4f8
ecabrera78 <ebcabrera78@gmail.com>
2017-01-10 23:47:51 -0600

    Initial creation
    Initial creation

---
+++ domchange.js
@@ -0,0 +1,10 @@
+/*
+* Complete the initScript function
+* Add an event listener to btnSubmit for click event
+* Create a new function to handle the click event
+*/
+
+function initScript() {
+    
+}
+
